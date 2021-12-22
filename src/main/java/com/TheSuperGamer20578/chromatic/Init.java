3c1d492545dbe8c323054e257774d74ededeb600
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-12-22 19:33:35 +1100

    Initialise JChroma and do wave effect

--- src/main/java/com/TheSuperGamer20578/chromatic/Init.java
+++ src/main/java/com/TheSuperGamer20578/chromatic/Init.java
@@ -1,11 +1,22 @@
 package com.TheSuperGamer20578.chromatic;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jglr.jchroma.JChroma;
+import org.jglr.jchroma.effects.WaveDirection;
+import org.jglr.jchroma.effects.WaveKeyboardEffect;
 
 
 public class Init implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("Chromatic");
+
     @Override
     public void onInitialize() {
         ModConfig.init();
+        JChroma chroma = JChroma.getInstance();
+        chroma.createKeyboardEffect(new WaveKeyboardEffect(WaveDirection.LEFT_TO_RIGHT));
+        LOGGER.info("Chromatic initialised");
     }
+
 }
