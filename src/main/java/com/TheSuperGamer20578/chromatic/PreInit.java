1b3693566b6d20e65b277b92effa8eda0416e43e
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-12-31 10:25:42 +1100

    Moved logger to Util.java

--- src/main/java/com/TheSuperGamer20578/chromatic/PreInit.java
+++ src/main/java/com/TheSuperGamer20578/chromatic/PreInit.java
@@ -1,17 +1,13 @@
 package com.TheSuperGamer20578.chromatic;
 
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jglr.jchroma.JChroma;
 
 public class PreInit implements PreLaunchEntrypoint {
-    public static final Logger LOGGER = LogManager.getLogger("Chromatic");
-
     @Override
     public void onPreLaunch() {
         JChroma chroma = JChroma.getInstance();
         chroma.init();
-        LOGGER.info("JChroma initialised");
+        Util.LOGGER.info("JChroma initialised");
     }
 }
---
+++ src/main/java/com/TheSuperGamer20578/chromatic/Util.java
@@ -0,0 +1,8 @@
+package com.TheSuperGamer20578.chromatic;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Util {
+    public static final Logger LOGGER = LogManager.getLogger("Chromatic");
+}
