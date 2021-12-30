0b7b3b57d6cca573eaa396ea1cd8857350ace7ee
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-12-31 10:27:25 +1100

    Changed layout so that colours update after being changed in the config

--- src/main/java/com/TheSuperGamer20578/chromatic/Layouts.java
+++ src/main/java/com/TheSuperGamer20578/chromatic/Layouts.java
@@ -1,21 +1,22 @@
 package com.TheSuperGamer20578.chromatic;
 
-import org.jglr.jchroma.utils.ColorRef;
-
 public class Layouts {
-    private static final ColourRef BGD = ColourRef.fromInt(ModConfig.INSTANCE.backgroundColour);
-    private static final ColourRef CHT = ColourRef.fromInt(ModConfig.INSTANCE.chatColour);
-    private static final ColourRef MOV = ColourRef.fromInt(ModConfig.INSTANCE.movementColour);
-    private static final ColourRef ESC = ColourRef.fromInt(ModConfig.INSTANCE.ESCColour);
-    private static final ColourRef TAB = ColourRef.fromInt(ModConfig.INSTANCE.tabColour);
-    private static final ColourRef MOD = ColourRef.fromInt(ModConfig.INSTANCE.modifierColour);
-    private static final ColourRef INV = ColourRef.fromInt(ModConfig.INSTANCE.inventoryColour);
-    public static final ColourRef[][] MAIN = {
-            {BGD, ESC, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD},
-            {BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD},
-            {BGD, TAB, INV, MOV, INV, BGD, CHT, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD},
-            {BGD, BGD, MOV, MOV, MOV, INV, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD},
-            {BGD, MOD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, CHT, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD},
-            {BGD, MOD, BGD, BGD, BGD, BGD, BGD, MOV, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD, BGD}
-    };
+    public static ColourRef[][] main() {
+        ModConfig config = ModConfig.INSTANCE;
+        ColourRef bgd = ColourRef.fromInt(config.backgroundColour);
+        ColourRef cht = ColourRef.fromInt(config.chatColour);
+        ColourRef mov = ColourRef.fromInt(config.movementColour);
+        ColourRef esc = ColourRef.fromInt(config.ESCColour);
+        ColourRef tab = ColourRef.fromInt(config.tabColour);
+        ColourRef mod = ColourRef.fromInt(config.modifierColour);
+        ColourRef inv = ColourRef.fromInt(config.inventoryColour);
+        return new ColourRef[][] {
+                {bgd, esc, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd},
+                {bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd},
+                {bgd, tab, inv, mov, inv, bgd, cht, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd},
+                {bgd, bgd, mov, mov, mov, inv, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd},
+                {bgd, mod, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, cht, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd},
+                {bgd, mod, bgd, bgd, bgd, bgd, bgd, mov, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd, bgd}
+        };
+    }
 }
