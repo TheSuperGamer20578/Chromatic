e3e72723995e5574cb8e7c1a6f2e7232fa30906f
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2023-04-12 15:31:52 +1000

    Added `/chromaticconfig` to open the config screen
    Closes #16

--- src/main/java/com/TheSuperGamer20578/chromatic/Init.java
+++ src/main/java/com/TheSuperGamer20578/chromatic/Init.java
@@ -1,6 +1,9 @@
 package com.TheSuperGamer20578.chromatic;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.minecraft.client.MinecraftClient;
 import org.jglr.jchroma.JChroma;
 import org.jglr.jchroma.effects.WaveDirection;
 import org.jglr.jchroma.effects.WaveKeyboardEffect;
@@ -12,6 +15,12 @@ public class Init implements ModInitializer {
         ModConfig.init();
         JChroma chroma = JChroma.getInstance();
         chroma.createKeyboardEffect(new WaveKeyboardEffect(WaveDirection.LEFT_TO_RIGHT));
+        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("chromaticconfig")
+            .executes(context -> {
+                MinecraftClient.getInstance().send(() -> MinecraftClient.getInstance().setScreen(AutoConfig.getConfigScreen(ModConfig.class, null).get()));
+                return 1;
+            })
+        );
         Util.LOGGER.info("Chromatic initialised");
     }
 
